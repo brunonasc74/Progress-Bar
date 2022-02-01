42c606b06e016b93b97deb5eb9a3af1855a75428
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-02-01 03:58:36 -0300

    layout created

--- app/src/main/java/com/example/progressbar/MainActivity.kt
+++ app/src/main/java/com/example/progressbar/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.progressbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inicialTextViewProgressY = textViewProgress.translationY
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                textViewProgress.text = progress.toString()
+
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
     }
 }
