b0e2a6238825e947c7036ea92e0df5349fdee726
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-02-01 07:53:21 -0300

    notification when button pressed added

--- app/src/main/java/com/example/progressbar/MainActivity.kt
+++ app/src/main/java/com/example/progressbar/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.progressbar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -37,6 +38,7 @@ class MainActivity : AppCompatActivity() {
 
         buttonReset.setOnClickListener { v ->
             seekBar.progress = 0
+            Toast.makeText(applicationContext, "Thx for pressing me ;)", Toast.LENGTH_SHORT).show()
         }
 
     }
